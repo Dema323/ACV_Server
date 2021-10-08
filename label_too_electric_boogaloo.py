f2a19b7e004c2d8fffb6de24b9534b90a46a6c6f
Reda Boussabat <reda.boussabat@hotmail.com>
2021-10-08 09:52:12 +0200

    comment

--- label_too_electric_boogaloo.py
+++ label_too_electric_boogaloo.py
@@ -7,6 +7,7 @@ def electric_boogaloo(filename='data/result.json'):
         for i in range(0, len(file_data['features'])):
             features = file_data['features']
             if(features[i]['type'] == 3 and features[i-1]['type'] == 1):
+                # type 1 bestaat nog niet
                     features[i]['type'] = 2
             else:
                 features[i]['type'] = 1
