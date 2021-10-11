85703594e8a7d8789de758b18acfb15dc494b333
Reda Boussabat <reda.boussabat@hotmail.com>
2021-10-11 08:47:24 +0200

    implemteren van methodes

--- label_stop_points.py
+++ label_stop_points.py
@@ -5,32 +5,47 @@ import location_visited_multiple_times
 
 
 
+# def add_label(filename='data/result.json'):
+#     with open(filename, 'r+') as file:
+#         file_data = json.load(file)
+#         for i in range(0, len(file_data['features'])):
+#             features = file_data['features']
+#             coordinate = features[i]['geometry']['coordinates']
+#             print(coordinate)
+#             if(not location_visited_multiple_times.location_visited_multiple_times(coordinate)):
+#                 if(backtracking.backtrackingcompare(features[i])):
+#                     features[i]['type'] = 3
+#                 #     if(point_already_restaurant == False):
+#                 #         features[i]['type']=1
+#                 #         point_already_restaurant = True
+#                 #     else:
+#                 #         features[i]['type']=2
+#                 #         point_already_restaurant = False
+#                 # else:
+#                 #     if(point_already_restaurant == False):
+#                 #         features[i]['type']=3
+#                 #         point_already_restaurant = True
+#                 #     else:
+#                 #         features[i]['type']=4
+#                 #         point_already_restaurant = False
+#             else:
+#                 features[i]['type']=5
+
+
 def add_label(filename='data/result.json'):
     with open(filename, 'r+') as file:
         file_data = json.load(file)
-        for i in range(0, len(file_data['features'])):
-            features = file_data['features']
-            coordinate = features[i]['geometry']['coordinates']
+        i = 1
+        for features in file_data['features']:
+            coordinate = features['geometry']['coordinates']
             print(coordinate)
-            if(not location_visited_multiple_times.location_visited_multiple_times(coordinate)):
-                if(backtracking.backtrackingcompare(features[i])):
-                    features[i]['type'] = 3
-                #     if(point_already_restaurant == False):
-                #         features[i]['type']=1
-                #         point_already_restaurant = True
-                #     else:
-                #         features[i]['type']=2
-                #         point_already_restaurant = False
-                # else:
-                #     if(point_already_restaurant == False):
-                #         features[i]['type']=3
-                #         point_already_restaurant = True
-                #     else:
-                #         features[i]['type']=4
-                #         point_already_restaurant = False
-            else:
-                features[i]['type']=5
-
+            if(i%2 == 1):
+                features['type']= 1
+            if(i%2 == 0):
+                features['type']= 2
+            i = i + 1
+        file.seek(0)
+        json.dump(file_data, file, indent=4)
 
 
         file.seek(0)
--- label_too_electric_boogaloo.py
+++ label_too_electric_boogaloo.py
@@ -1,16 +1,20 @@
 import json
 from geopy import distance
+from backtracking import backtrackingcompare
 
 def electric_boogaloo(filename='data/result.json'):
     with open(filename, 'r+') as file:
         file_data = json.load(file)
         for i in range(0, len(file_data['features'])):
             features = file_data['features']
-            if(features[i]['type'] == 3 and features[i-1]['type'] == 1):
-                # type 1 bestaat nog niet
-                    features[i]['type'] = 2
-            else:
-                features[i]['type'] = 1
+            if(backtrackingcompare(features[i]) == True):
+                if(features[i-1]['type'] == 2):
+                    features[i]['type'] = 3
+                else:
+                    features[i]['type'] = 4
+                print(features[i])
+        file.seek(0)
+        json.dump(file_data, file, indent=4)
 
 
 electric_boogaloo()
